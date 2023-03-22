7eff37993dcf88ab460f34d6bb3cf9c8bddd2e25
Thitiphat <thitiphat.tana@kmutt.ac.th>
2023-03-22 18:06:12 +0700

    second commands

--- Myproject1.java
+++ Myproject1.java
@@ -15,6 +15,7 @@ public class Myproject1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello Java");
     }
     
 }
